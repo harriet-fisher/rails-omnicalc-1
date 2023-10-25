2fabf1fe1f422e958a3b66e562a8f64320d807bd
harriet-fisher <106377699+harriet-fisher@users.noreply.github.com>
2023-10-25 00:09:34 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get("/", {:controller => "calculator", :action => "home"})
+  get("/square/new", {:controller => "calculator", :action => "home"})
   post("/square/results", {:controller => "calculator", :action => "square_results"})
-  get("/square_root", {:controller => "calculator", :action => "square_root"})
+  get("/square_root/new", {:controller => "calculator", :action => "square_root"})
   post("/square_root/results", {:controller => "calculator", :action => "square_root_results"})
-  get("/payment", {:controller => "calculator", :action => "payment"})
+  get("/payment/new", {:controller => "calculator", :action => "payment"})
   post("/payment/results", {:controller => "calculator", :action => "payment_results"})
-  get("/random", {:controller => "calculator", :action => "random"})
+  get("/random/new", {:controller => "calculator", :action => "random"})
   post("/random/results", {:controller => "calculator", :action => "random_results"})
 end
